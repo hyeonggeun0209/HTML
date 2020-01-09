7b7725aaada82d6b0240c2fcc276c78a8e5c336c
Jihg <enddj99@naver.com>
2020-01-10 00:15:08 +0900

    first version

---
+++ PHP/number.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Number & Arithmetic Operator</h1>
+    <h2>1+1</h2>
+    <?php
+      echo 1+1;
+     ?>
+    <h2>2-1</h2>
+    <?php
+      echo 2-1;
+    ?>
+    <h2>2*2</h2>
+    <?php
+      echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?php
+      echo 4/2;
+    ?>
+    <h2>7%3</h2>
+    <?php
+      echo 7%3;
+    ?>
+
+  </body>
+</html>
---
+++ PHP/variable.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "hahah";
+    echo "Lorem ipsum dolor sit amet,
+    consectetur adipisicing elit,
+    sed do".$name." smod tempor incididunt ut labore et
+    dolore magna aliqua. Ut ".$name." enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo ".$name." consequat. Duis aute irure dolor
+    in reprehenderit in ".$name." voluptate velit esse cillum dolore
+    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    non proident, sunt in culpa ".$name." qui officia deserunt mollit
+    anim id est laborum."
+    ?>
+
+  </body>
+</html>

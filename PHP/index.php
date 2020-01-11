4e09bc532792d889d8506ce4f206d946b38e0448
Jihg <enddj99@naver.com>
2020-01-12 00:08:18 +0900

    second version

--- PHP/index.php
+++ PHP/index.php
@@ -1,23 +1,53 @@
+<?php
+  function print_title() {
+    if (isset($_GET['id'])) {
+      echo $_GET['id'];
+    } else {
+      echo "Welcome";
+    }
+  }
+  function print_description() {
+    if (isset($_GET['id'])) {
+      echo file_get_contents("data/".$_GET['id']);
+    } else {
+      echo "Hello, PHP";
+    }
+  }
+  function print_list() {
+    $list = scandir("./data");
+    $i = 0;
+    while ($i < count($list)) {
+      if($list[$i] !== "." && $list[$i] !== "..") {
+        echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+      }
+      $i++;
+    }
+  }
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>
+      <?php
+        print_title();
+      ?>
+    </title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href = "index.php">WEB</a></h1>
     <ol>
-      <li><a href = "index.php?id=HTML">HTML</a></li>
-      <li><a href = "index.php?id=CSS">CSS</a></li>
-      <li><a href = "index.php?id=JavaScript">JavaScript</a></li>
+      <?php
+        print_list();
+      ?>
     </ol>
     <h2>
       <?php
-        echo $_GET['id'];
+        print_title();
       ?>
     </h2>
     <?php
-      echo file_get_contents("data/".$_GET['id']);
+      print_description();
     ?>
   </body>
 </html>

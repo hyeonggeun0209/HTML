95a75df9fc4be6509752e6cbbfc8ba431c6bbafa
hyeonggeun0209 <enddj99@naver.com>
2022-01-02 17:38:13 +0900

    first commit

---
+++ Python/syntax/string.py
@@ -0,0 +1,20 @@
+print("hello world")
+print('hello world')
+print("Hell'o' world")
+#escape
+print("Hell'o' \"w\"orld")
+#newline
+print('H')
+print('e')
+print('l')
+print('l')
+print('o')
+print('H\ne\nl\nl\no')
+#docstring
+print('''
+H
+e
+l
+l
+o
+''')
